1216f05d505b8ae505dfd654cb381a4cd4645594
Ahmet Kaya <ahka1143@gmail.com>
2022-10-27 19:26:58 +0300

    27.10.2022

--- src/test/java/tests/KayitliKullanici.java
+++ src/test/java/tests/KayitliKullanici.java
@@ -1,5 +1,6 @@
 package tests;
 
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.interactions.Actions;
 import pages.HepsiBuradaPages;
@@ -11,11 +12,12 @@ import java.util.Set;
 
 import static org.testng.Assert.*;
 
-public class Test {
+public class KayitliKullanici {
 
 
     @org.testng.annotations.Test
-    public void kullaniciGirisliTest() {
+    public void kayitliKullaniciTest() {
+
         HepsiBuradaPages hepsiBuradaPages = new HepsiBuradaPages();
         Actions actions = new Actions(Driver.getDriver());
         Driver.getDriver().get(ConfigReader.getProperty("hepsiburadaUrl"));
@@ -59,42 +61,5 @@ public class Test {
 
     }
 
-    @org.testng.annotations.Test
-    public void kullaniciGirissizTest() {
-        Driver.getDriver().quit();
-        HepsiBuradaPages hepsiBuradaPages = new HepsiBuradaPages();
-        Driver.getDriver().get(ConfigReader.getProperty("hepsiburadaUrl"));
 
-        ReusableMethods.waitForClickablility(hepsiBuradaPages.aramaKutusu, 10);
-        hepsiBuradaPages.aramaKutusu.click();
-        hepsiBuradaPages.aramaKutusu.sendKeys("kalem", Keys.ENTER);
-        String ilkSayfaWindowHandle = Driver.getDriver().getWindowHandle();
-        ReusableMethods.waitForClickablility(hepsiBuradaPages.ilkUrun, 10);
-        ReusableMethods.scroolDowntoPixel(300);
-        hepsiBuradaPages.ilkUrun.click();
-        Set<String> windowHandlesSet = Driver.getDriver().getWindowHandles();
-        for (String each : windowHandlesSet
-        ) {
-            if (each != ilkSayfaWindowHandle) {
-                Driver.getDriver().switchTo().window(each);
-            }
-        }
-        String urunAdi = hepsiBuradaPages.urunAdiText.getText();
-        System.out.println(urunAdi);
-        hepsiBuradaPages.cerezKabulEt.click();
-        ReusableMethods.waitForClickablility(hepsiBuradaPages.sepeteEkleList.get(0), 5);
-        ReusableMethods.scroolDowntoPixel(800);
-        hepsiBuradaPages.sepeteEkleList.get(0).click();
-        ReusableMethods.waitForClickablility(hepsiBuradaPages.sepeteEkleList.get(1), 10);
-        hepsiBuradaPages.sepeteEkleList.get(1).click();
-        ReusableMethods.waitForVisibility(hepsiBuradaPages.sepeteGitButonu, 5);
-        hepsiBuradaPages.sepeteGitButonu.click();
-
-        String sepettekiIlkUrunText = hepsiBuradaPages.sepettekiUrunlerList.get(0).getText();
-        String sepettekiIkinciUrunText = hepsiBuradaPages.sepettekiUrunlerList.get(1).getText();
-        System.out.println(sepettekiIlkUrunText);
-        System.out.println(sepettekiIkinciUrunText);
-        assertTrue(sepettekiIlkUrunText.contains(urunAdi) && sepettekiIkinciUrunText.contains(urunAdi));
-        Driver.getDriver().quit();
-    }
 }
